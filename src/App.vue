e6f10026199f8f8fbc64b6e5fda8137a719fc395
kylian <kylian.cadet@epitech.eu>
2020-04-06 16:17:24 +0200

    reorganize basic vuetify files 	modified:   src/App.vue 	new file:   src/components/Banner.vue 	modified:   src/views/Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <v-app>
-    <router-view></router-view>
+    <!-- Top Banner -->
+    <Banner/>
+    <v-content>
+      <!-- Content set to the router view (see @router/views.js) -->
+      <router-view/>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import Banner from './components/Banner';
 
 export default {
-  name: "App",
+  name: 'App',
+
+  components: {
+    Banner
+  },
+
   data: () => ({
     //
-  })
+  }),
 };
 </script>
