5c04780eabfcc39ac6fd42a605e7b858bd6a3ea5
kylian <kylian.cadet@epitech.eu>
2020-04-07 01:27:44 +0200

    add a component example wich show the total confirmed modify title tag 	modified:   package.json 	modified:   public/index.html 	new file:   src/components/TotalConfirmed.vue 	modified:   src/views/Home.vue

---
+++ src/components/TotalConfirmed.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-flex style="background-color: black">
+    <h3 style="color: white">Total Confirmed</h3>
+    <h1 style="color: red">{{total}}</h1>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: "TotalConfirmed",
+  props: ["data"],
+  data: function() {
+    return {
+      total: 0
+    }
+  },
+  // Watcher on data props to re-calc when fetching is done
+  watch: {
+    data: function() {
+      this.calc()
+    }
+  },
+  created() {
+    this.calc()
+  },
+  methods: {
+    calc : function() {
+      var total = 0
+      for (var i = 0; i != this.data.length; i++) {
+        total += Number(this.data[i]["attributes"]["Confirmed"])
+      }
+      this.total = total
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div>
+    <v-snackbar v-model="error">
+      <p>Failed to fetch Data</p>
+      <v-btn color="red" @click="error = false">Close</v-btn>
+    </v-snackbar>
     <router-link to="/about">Go to About</router-link>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TotalConfirmed v-bind:data="data" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TotalConfirmed from "@/components/TotalConfirmed";
+var url = require("url");
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    TotalConfirmed
+  },
+  data: function() {
+    return {
+      data: [],
+      error: false
+    };
+  },
+  created() {
+    this.fetchData()
+    // setInterval(this.fetchData, 1000 * 10) For hot reload
+  },
+  methods: {
+    fetchData: function() {
+      alert("in fetch data")
+      var payload = {
+        f: "json",
+        where: "Confirmed > 0",
+        returnGeometry: "false",
+        outFields: "*",
+        orderByFields: "Confirmed desc",
+        resultRecordCount: "1000"
+      };
+      var query = url.format({ query: payload });
+      fetch(
+        "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query" +
+          query
+      )
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          if (data.error) {
+            this.error = true;
+            return;
+          }
+          this.data = data["features"];
+        })
+        .catch((/* error */) => {
+          this.error = true;
+        });
+    }
   }
 };
 </script>
