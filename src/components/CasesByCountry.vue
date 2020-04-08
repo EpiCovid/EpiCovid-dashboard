662758266ecd2f930a5cd859ef60fa2ea419fc48
Axel Conceicao <axel.conceicao@epitech.eu>
2020-04-09 00:50:39 +0200

    cases by country done

--- src/components/CasesByCountry.vue
+++ src/components/CasesByCountry.vue
@@ -1,44 +1,73 @@
 <template>
     <v-card id="box" class="pa-0 ma-1">
-        <v-layout column fill-height class="pa-0 ma-0 my-2">
-            <h2 class="main-color center">Cases by Country</h2>
+        <v-layout column fill-height class="pa-0 ma-0">
+            <h2 class="my-2 main-color center">Cases by Country</h2>
+            <v-layout column style="height: 0vh;">
+                <v-flex style="overflow: auto;">
+                    <v-list class="overflow-y-auto">
+                        <v-layout column>
+                            <span class="row-border py-1" v-for="country of countries" :key="country.Country_Region">
+                                <span class="red-color ml-3" style="font-weight: bold;">
+                                    {{ country.Confirmed }}
+                                </span>
+                                <span>{{ country.Country_Region }}</span>
+                            </span>
+                        </v-layout>
+                    </v-list>
+                </v-flex>
+            </v-layout>
         </v-layout>
     </v-card>
 </template>
 
 <script>
 export default {
-    name: "CasesByCountry",
-    props: ["data"],
-    data: function() {
+    name: 'CasesByCountry',
+    props: ['data'],
+    data: function () {
         return {
-            total: 0,
+            countries: [],
+            size: this.getWindowSize(),
+            headers: [
+                { text: 'Confirmed', value: 'Confirmed', align: 'right' },
+                { text: 'Country_Region', value: 'Country_Region' },
+            ],
         };
     },
     // Watcher on data props to re-calc when fetching is done
     watch: {
-        data: function() {
+        data: function () {
             this.calc();
-        }
+        },
     },
     created() {
         this.calc();
     },
     methods: {
-        numberWithSpaces: function(x) {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        numberWithSpaces: function (x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
         },
-        calc: function() {
-            var total = 0;
+        getWindowSize: function () {
+            return { x: window.innerWidth, y: window.innerHeight };
+        },
+        calc: function () {
+            var countries = [];
             for (var i = 0; i != this.data.length; i++) {
-                total += Number(this.data[i]["attributes"]["Confirmed"]);
+                var tmp = false;
+                countries.forEach((item) => {
+                    if (item.Country_Region === this.data[i]['attributes']['Country_Region']) {
+                        item.Confirmed = parseInt(item.Confirmed) + parseInt(this.data[i]['attributes']['Confirmed']);
+                        tmp = true;
+                    }
+                });
+                if (!tmp) countries.push(this.data[i]['attributes']);
             }
-            this.total = this.numberWithSpaces(total);
-        }
-    }
+            this.countries = countries;
+        },
+    },
 };
 </script>
 
 <style>
-@import "../assets/styles/style.css";
+@import '../assets/styles/style.css';
 </style>
