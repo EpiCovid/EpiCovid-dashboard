503c4bdf5bf8fccaaec0bea505d44a577feda65c
Axel Conceicao <axel.conceicao@epitech.eu>
2020-04-09 15:55:37 +0200

    last update component done

--- src/components/LastUpdate.vue
+++ src/components/LastUpdate.vue
@@ -2,44 +2,49 @@
     <v-card id="box" class="pa-0 ma-1">
         <v-layout column fill-height class="justify-center align-center pa-0 ma-0">
             <h3 class="main-color center" style="line-height: 1.2em; font-weight: normal;">Last Update</h3>
-            <h3 class="main-color center" style="line-height: 1.2em; font-weight: bold;">01/01/2000</h3>
+            <h3 class="main-color center" style="line-height: 1.2em; font-weight: bold;">{{ date }}</h3>
         </v-layout>
     </v-card>
 </template>
 
 <script>
 export default {
-    name: "LastUpdate",
-    props: ["data"],
-    data: function() {
+    name: 'LastUpdate',
+    props: ['data'],
+    data: function () {
         return {
-            total: 0,
+            date: 'h',
         };
     },
     // Watcher on data props to re-calc when fetching is done
     watch: {
-        data: function() {
+        data: function () {
             this.calc();
-        }
+        },
     },
     created() {
         this.calc();
     },
     methods: {
-        numberWithSpaces: function(x) {
-            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        convertTimestamp: function (ts) {
+            var dateFormat = require('dateformat');
+            var date = new Date(ts);
+            return dateFormat(date, 'mm/dd/yyyy h:MM TT');
         },
-        calc: function() {
-            var total = 0;
+        calc: function () {
+            var tab = [];
             for (var i = 0; i != this.data.length; i++) {
-                total += Number(this.data[i]["attributes"]["Confirmed"]);
+                tab.push(Number(this.data[i]['attributes']['Last_Update']));
             }
-            this.total = this.numberWithSpaces(total);
-        }
-    }
+            var tmp = tab.sort(function (a, b) {
+                return parseInt(b) - parseInt(a);
+            })[0];
+            this.date = this.convertTimestamp(tmp);
+        },
+    },
 };
 </script>
 
 <style>
-@import "../assets/styles/style.css";
+@import '../assets/styles/style.css';
 </style>
