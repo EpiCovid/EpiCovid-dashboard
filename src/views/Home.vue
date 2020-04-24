3827f3fe7f79c43a8e80ab8dbab4451165b20686
Kylian <kylian.cadet@epitech.eu>
2020-04-24 16:10:31 +0200

    fix Chart component height 	modified:   src/views/Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
       </v-card>
       <v-row class="pa-0 ma-0">
         <v-layout column class="pa-0 ma-0 col-2">
-          <TotalConfirmed style="height: 15%;" :data="data" />
+          <TotalConfirmed style="height: 15vh" :data="data" />
           <CasesByCountry style="max-height: 78%;height: 78%;" :data="data" />
           <LastUpdate style="height: 7%;" :data="data" />
         </v-layout>
@@ -42,7 +42,7 @@
               <TotalRecovered style="height:100%;" :data="data" />
             </v-layout>
           </v-layout>
-          <Charts style="height:40%;" :data="data" />
+          <Charts style="height:40vh;" :data="data" />
         </v-layout>
       </v-row>
     </v-layout>
