e3c15c8a621ed2fb36c985572ff120bc4ac34b43
Kylian <kylian.cadet@epitech.eu>
2020-04-28 17:50:13 +0200

    add keyword input in GoogleTrends component 	modified:   src/components/GoogleTrends.vue

--- src/components/GoogleTrends.vue
+++ src/components/GoogleTrends.vue
@@ -1,5 +1,17 @@
 <template>
   <v-flex>
+    <v-layout justify-center>
+      <div>
+      <v-text-field
+        label="Keyword"
+        v-model="keyword"
+        append-icon="mdi-magnify"
+        class="pt-4"
+        @click:append="load()"
+        v-on:keyup.enter="load()"
+      ></v-text-field>
+      </div>
+    </v-layout>
     <div id="google"></div>
   </v-flex>
 </template>
@@ -12,10 +24,18 @@ export default {
     data: function() {
         return {
             total: 0,
+            keyword: "Coronavirus"
         };
     },
     mounted: function() {
-      trends.embed.renderExploreWidgetTo(document.getElementById("google"), "TIMESERIES", { "comparisonItem": [{ "keyword": "Coronavirus", "geo": "", "time": "today 3-m" }], "category": 0, "property": "" }, { "exploreQuery": "date=today%203-m&q=Coronavirus", "guestPath": "https://trends.google.com:443/trends/embed/" });
+      this.load()
     },
+    methods: {
+      load() {
+        const div = document.getElementById("google");
+        div.innerHTML = "";
+        trends.embed.renderExploreWidgetTo(document.getElementById("google"), "TIMESERIES", { "comparisonItem": [{ "keyword": this.keyword, "geo": "", "time": "today 3-m" }], "category": 0, "property": "" }, { "exploreQuery": "date=today%203-m&q=" + this.keyword, "guestPath": "https://trends.google.com:443/trends/embed/" });
+      }
+    }
 };
 </script>
